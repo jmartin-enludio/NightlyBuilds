9d9c117cc5ee5d5f05f4a91c665bbd3bb56de17a
jmartin-enludio <jordan.martin@enludio.com>
2026-06-11 14:21:38 -0400

    Add NightlyBuilds unreal project

---
+++ Source/NightlyBuilds/NightlyBuilds.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "NightlyBuilds.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NightlyBuilds, "NightlyBuilds" );
---
+++ Source/NightlyBuilds/NightlyBuildsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NightlyBuildsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NIGHTLYBUILDS_API ANightlyBuildsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
